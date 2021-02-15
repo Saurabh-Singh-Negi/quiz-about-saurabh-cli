5e3b43200a5d47ba86dfb10fa3c16857efe2a792
saurabh negi <saurabhnegi0007@gmail.com>
2021-02-15 13:57:33 +0530

    update: added function to check answers and calculate score

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+var readlineSync = require("readline-sync");
+
+let userName = readlineSync.question("What's your name? ");
+console.log("Let's begin the game", userName);
+
+var score = 0;
+function check(question,answer) {
+  let userAns = readlineSync.question(question);
+
+  if(userAns === answer){
+    score++;
+    console.log("You are right!");
+  }
+  else {
+    console.log("You got it wrong!");
+  }
+
+  console.log("current score:",score);
+  console.log("-----------------");
+} 
+
 var questionArr = [
     {
       question:"who is my favourite superhero? ",
