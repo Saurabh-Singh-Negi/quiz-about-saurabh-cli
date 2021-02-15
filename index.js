1dfe6a198d85559628ae1816b873872156b14c92
saurabh negi <saurabhnegi0007@gmail.com>
2021-02-15 13:54:19 +0530

    added questions

---
+++ index.js
@@ -0,0 +1,15 @@
+var questionArr = [
+    {
+      question:"who is my favourite superhero? ",
+      ans:"superman"
+    },
+    {
+      question:"what sport do i play? ",
+      ans:"football"
+    },
+    {
+      question:"where do I live? ",
+      ans:"odisha"
+    }
+    
+  ];
